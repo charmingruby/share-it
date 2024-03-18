50809d602bbbd43bf1133fe63fba81a3cad06f75
charmingruby <mrrubycanal3105@gmail.com>
2024-03-18 13:06:00 -0300

    feat: base e2e tests

--- share-it/src/controllers/create-question.e2e-spec.ts
+++ share-it/src/controllers/create-question.e2e-spec.ts
@@ -1,12 +1,14 @@
 import { AppModule } from '@/app.module';
 import { PrismaService } from '@/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
 
 describe('Create account (E2E)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -16,25 +18,38 @@ describe('Create account (E2E)', () => {
     app = moduleRef.createNestApplication();
 
     prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
 
   test('[POST] /accounts', async () => {
-    const response = await request(app.getHttpServer()).post('/accounts').send({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '12345678',
-    });
-
-    expect(response.statusCode).toBe(201);
-
-    const userOnDatabase = await prisma.user.findUnique({
-      where: {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
         email: 'johndoe@example.com',
+        password: '12345678',
       },
     });
 
-    expect(userOnDatabase).toBeTruthy();
+    const accessToken = jwt.sign({ sub: user.id });
+
+    const response = await request(app.getHttpServer())
+      .post('/questions')
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send({
+        title: 'New question',
+        content: 'Question content',
+      });
+
+    expect(response.statusCode).toBe(201);
+
+    const questionOnDatabase = await prisma.question.findFirst({
+      where: {
+        title: 'New question',
+      },
+    });
+
+    expect(questionOnDatabase).toBeTruthy();
   });
 });
