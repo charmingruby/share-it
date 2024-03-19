cb9ebaae955630ca819cf3cede642f1920928673
charmingruby <mrrubycanal3105@gmail.com>
2024-03-18 21:53:39 -0300

    feat: infra layer organization

--- src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -1,9 +1,9 @@
-import { AppModule } from '@/app.module';
-import { PrismaService } from '@/prisma/prisma.service';
+import { PrismaService } from '@/infra/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { JwtService } from '@nestjs/jwt';
 import request from 'supertest';
+import { AppModule } from '../../app.module';
 
 describe('Fetch recent questions (E2E)', () => {
   let app: INestApplication;
