18e3134be3e8855785d56f82a835882788147f2f
charmingruby <mrrubycanal3105@gmail.com>
2024-03-21 09:14:08 -0300

    feat: prisma question mapper and answers table

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -8,13 +8,13 @@ import { QuestionBestAnswerChosenEvent } from '@/domain/notification/enterprise/
 
 export interface QuestionProps {
   title: string;
-  bestAnswerId?: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID | null;
   attachments: QuestionAttachmentList;
   content: string;
   slug: Slug;
   authorId: UniqueEntityID;
   createdAt: Date;
-  updatedAt?: Date;
+  updatedAt?: Date | null;
 }
 
 export class Question extends AggregateRoot<QuestionProps> {
@@ -74,15 +74,8 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.touch();
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-    if (bestAnswerId === undefined) {
-      return;
-    }
-
-    if (
-      this.props.bestAnswerId === undefined ||
-      !this.props.bestAnswerId.equals(bestAnswerId)
-    ) {
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
+    if (bestAnswerId && bestAnswerId != this.props.bestAnswerId) {
       this.addDomainEvent(
         new QuestionBestAnswerChosenEvent(this, bestAnswerId),
       );
